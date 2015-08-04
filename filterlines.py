d1e18fb813ba2b994efbb6d60bd37b8d1d8add45
Ihor Bobak <ibobak@gmail.com>
2015-08-04 16:47:39 +0300

    The script for filtering lines

---
+++ filterlines.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from optparse import OptionParser
+import sys
+
+parser = OptionParser()
+parser.add_option('-f', '--filter', dest='filter', help='Filter for strings (list of strings which WON''T go into the output)', metavar='FILTER')
+args, inputfile = parser.parse_args()
+if len(inputfile) > 1:
+    print "Format: filterlines.py [-f filter.txt] [inputfile.txt]"
+    sys.exit(255)
+
+# build the filtering set
+filter_exclude = set()
+if args.filter:
+    with open(args.filter) as f:
+        for s in f:
+            filter_exclude.add(s.rstrip())
+
+# read the file and output filtered lines
+if len(inputfile) == 1:
+    sys.stdin = open(inputfile[0], "r")
+
+for line in sys.stdin.readlines():
+    found = False
+    for filter_string in filter_exclude:
+        if filter_string in line:
+            found = True
+            break
+    if not found:
+        print(line.rstrip())
